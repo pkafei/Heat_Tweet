6848a6878ebd05841da6e72377be5df5d0f0f98a
Pkafei <plburton@gmail.com>
2013-07-23 20:02:05 -0700

    Added login, logout, and unauthorized

--- hello.py
+++ hello.py
@@ -11,16 +11,53 @@ oauth = OAuth(app)
 
 twitter = oauth.remote_app(
     'twitter',
-    consumer_key='  ',
-    consumer_secret=' ',
+    consumer_key='OwdDOkUp4aI751Xnm7fOg  ',
+    consumer_secret='dfPYN1ZeKEm36prWGYCVqj5RFkGZgu3WLXXwuIIKmUQ',
     base_url= ' ',
-    request_token_url=' ',
-    access_token_url=' ',
-    authorize_url=' ',
+    request_token_url='https://api.twitter.com/oauth/request_token',
+    access_token_url='https://api.twitter.com/oauth/access_token',
+    authorize_url='https://api.twitter.com/oauth/authenticate',
+)
 
 #@app.route('/')
 #def hello():
    # return "Awesomeness this is going to be the one."
+
+
+@twitter.tokengetter
+def get_twitter_token():
+    if 'twitter_oauth' in session:
+        resp = session['twitter_oauth']
+        return resp['oauth_token'], resp['oauth_token_secret']
+
+
+@app.before_request
+def before_request():
+    g.user = None
+    if 'twitter_oauth' in session:
+        g.user = session['twitter_oauth']
+
+
+@app.route('/login')
+def login():
+    callback_url = url_for('oauthorized', next=request.args.get('next'))
+    return twitter.authorize(callback=callback_url or request.referrer or None)
+
+
+@app.route('/logout')
+def logout():
+    session.pop('twitter_oauth', None)
+    return redirect(url_for('index'))
+
+
+@app.route('/oauthorized')
+@twitter.authorized_handler
+def oauthorized(resp):
+    if resp is None:
+        flash('Authentication Error!')
+    else:
+        session['twitter_oauth'] = resp
+    return redirect(url_for('index'))
 
 
 @app.route('/')
