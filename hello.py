943a4686b011fa71ad05f7ef31cc13898493e9ea
Pkafei <plburton@gmail.com>
2013-07-23 18:01:54 -0700

    Twitter bootstrap added to base.html

--- hello.py
+++ hello.py
@@ -1,9 +1,18 @@
-import os
-from flask import Flask
+from flask import Flask, render_template, json, request
 
 app = Flask(__name__)
 
-@app.route('/')
-def hello():
-    return "Awesomeness this is going to be the one."
 
+#@app.route('/')
+#def hello():
+   # return "Awesomeness this is going to be the one."
+
+
+@app.route('/')
+def index():
+    """ Just a generic index page to show."""
+    return render_template('base.html')
+
+
+if __name__ == '__main__':
+    app.run()
