d6c82cfdf21adf39289b75c9e17a7758512a25de
Pkafei <plburton@gmail.com>
2013-07-23 19:24:40 -0700

    Add some twitter goodness

--- hello.py
+++ hello.py
@@ -1,7 +1,22 @@
 from flask import Flask, render_template, json, request
+from flask_oauthlib.client import OAuth
+from flask import g, session, request, url_for, flash
+from flask_oauthlib.client import OAuth
 
 app = Flask(__name__)
+app.debug = True
+app.secret_key = 'Write Something!'
 
+oauth = OAuth(app)
+
+twitter = oauth.remote_app(
+    'twitter',
+    consumer_key='  ',
+    consumer_secret=' ',
+    base_url= ' ',
+    request_token_url=' ',
+    access_token_url=' ',
+    authorize_url=' ',
 
 #@app.route('/')
 #def hello():
